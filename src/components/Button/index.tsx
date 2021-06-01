aa4af0ef4fcbc72b9ef2fa8132845755fe6573b4
somayehbhr <somayebhr@gmail.com>
2021-06-02 01:17:31 +0430

    feat: added Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,15 @@
 import React, { MouseEventHandler } from "react";
-import data from "../../data/info.json";
+
 interface DeleteBooksProps {
 	onClick: MouseEventHandler<HTMLButtonElement>;
 	text: string;
 }
-export const DeleteBooks: React.FC<DeleteBooksProps> = (props) => {
+const Button: React.FC<DeleteBooksProps> = (props) => {
 	return (
 		<button type="button" className="btn btn-danger" onClick={props.onClick}>
 			{props.text}
 		</button>
 	);
 };
+
+export default Button
