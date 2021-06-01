745480dd57667875183a4002e117d593904bdf5b
somayehbhr <somayebhr@gmail.com>
2021-06-02 01:19:26 +0430

    chore: make some changes on project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import "./App.css";
-import { Books } from "./components/Books";
+import React from "react";
+import BookList from "./screens/BookList"
+import Navbar from "./components/Navbar";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+} from "react-router-dom";
 
 export function App() {
 	return (
 		<div className="App">
-			<Books />
+
+			<Navbar/>
+			<BookList/>
 		</div>
 	);
 }
