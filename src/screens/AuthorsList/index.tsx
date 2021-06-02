8567d2c062bf2f94db009f772337d43541c7ef45
Somayeh Bahiraie <somayebhr@gmail.com>
2021-06-02 11:25:19 +0430

    feat: added AuthorList component

---
+++ src/screens/AuthorsList/index.tsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header";
+import React from "react";
+
+
+const headerList = ["fullName","number Of Books"]
+const AuthorsList = () => {
+   return(
+       <table>
+           <Header list={headerList}/>
+       </table>
+   )
+}
+export default AuthorsList
