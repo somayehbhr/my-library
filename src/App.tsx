4c10f1d70d8e0abdaac93d0395191dd912877473
Somayeh Bahiraie <somayebhr@gmail.com>
2021-06-14 20:08:58 +0430

    chore: minor improvement

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,33 @@
 import React from "react";
-import { connect } from "react-redux";
 import Screens from "./screens";
 import { AuthorEntity, AuthorState } from "./store/Authors/author.reducer";
-import { Dispatch } from "redux";
 import { StateNetwork } from "./types/store.type";
+import { useDispatch, useSelector } from "react-redux";
 
-interface AppProps {
-	currentId: number;
-	list: AuthorEntity[];
-	addAuthor(author: AuthorEntity): void;
-	updateCurrentId(id: number): void;
-}
+const App: React.FC = () => {
+	const authors = useSelector<StateNetwork, Array<AuthorEntity>>((state) => state.authors.list);
+	const dispatch = useDispatch();
+	console.count("rerender");
 
-const App: React.FC<AppProps> = (props) => {
-	function handleAddNewAuthor() {
-		props.addAuthor({
-			id: 1,
-			fullName: "Somayeh Bahiraei",
+	function addAuthor(author: AuthorEntity) {
+		dispatch({
+			type: "ADD_AUTHOR",
+			payload: author,
 		});
 	}
-	console.count("rerender");
+
+	function updateCurrentId(id: number) {
+		dispatch({
+			type: "UPDATE_CURRENT_ID",
+			payload: id,
+		});
+	}
+
 	return (
 		<div className="App">
-			<button onClick={() => props.updateCurrentId(3)}>Update current id</button>
-			<button onClick={handleAddNewAuthor}>Add New Author</button>
-			{JSON.stringify(props.list, null, 2)}
-			{/*<Screens />*/}
+			<Screens />
 		</div>
 	);
 };
 
-function mapStateToProps(state: StateNetwork) {
-	console.log(state);
-	return {
-		list: state.authors.list,
-		currentId: state.authors.currentId,
-	};
-}
-function mapDispatchToProps(dispatch: Dispatch) {
-	return {
-		addAuthor(author: AuthorEntity) {
-			dispatch({
-				type: "ADD_AUTHOR",
-				payload: author,
-			});
-		},
-		updateCurrentId(id: number) {
-			dispatch({
-				type: "UPDATE_CURRENT_ID",
-				payload: id,
-			});
-		},
-	};
-}
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
